3e6ade3faeda379eb4c590b1691ca6d0ff0771bf
Pedro Ferreira <pedrobvf@hotmail.com>
2024-10-01 20:10:06 +0000

    fix errors

--- src/main/java/com/user/usertest/exception/ErrorHandlingControllerAdvice.java
+++ src/main/java/com/user/usertest/exception/ErrorHandlingControllerAdvice.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @ControllerAdvice
 public class ErrorHandlingControllerAdvice {
 
